3cc1d2bd920f618e05a752e9f78b267fc22d26e3
Rob Cabacungan <rob.cabacungan@confirm.com>
2023-06-05 20:31:56 -0400

    feat: setup on ports 9121, 9122

---
+++ Makefile
@@ -0,0 +1,6 @@
+apisrv:
+	poetry run python manage.py runserver 0.0.0.0:9121
+
+webapp:
+	cd frontend ; npm run dev
+
--- app/settings.py
+++ app/settings.py
@@ -131,7 +131,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, "app", "staticfiles")
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "app", "static"),)
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "app", "static"),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
