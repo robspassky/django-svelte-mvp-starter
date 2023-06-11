e8606ad5c115dd73f33487355e07764759cf0bfe
Rob Cabacungan <rob.cabacungan@confirm.com>
2023-06-11 19:51:08 -0400

    feat: add env variables + dashboard

--- Makefile
+++ Makefile
@@ -1,6 +1,11 @@
 apisrv:
-	poetry run python manage.py runserver 0.0.0.0:9121
+	DJANGO_DEBUG=ON poetry run python manage.py runserver 0.0.0.0:9121
 
 webapp:
 	cd frontend ; npm run dev
+
+initial:
+	poetry install
+	poetry run python manage.py collectstatic
+	poetry run python manage.py createsuperuser
 
--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,41 +1,47 @@
 <script>
-	import { onMount } from 'svelte';
-	export let name;
+  import { Maintenance } from "flowbite-svelte-admin-dashboard";
+  import { onMount } from "svelte";
+  export let name;
 
-	let apimessage = "Waiting for server...";
+  let apimessage = "Waiting for server...";
 
-	onMount(async () => {
-		let resp = await fetch('/api/greet').then((res) => res.json());
-		console.log(resp);
-		apimessage = JSON.stringify(resp);
-	});
+  onMount(async () => {
+    let resp = await fetch("/api/greet").then((res) => res.json());
+    console.log(resp);
+    apimessage = JSON.stringify(resp);
+  });
 </script>
 
 <main>
-	<h1>who dat {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-	<h3>Data from server</h3>
-	{apimessage}
+  <h1>somewhat dis {name}!</h1>
+  <p>
+    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+    how to build Svelte apps. hello there
+  </p>
+  <h3>Data from server</h3>
+  {apimessage}
+  <Maintenance />
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
+
